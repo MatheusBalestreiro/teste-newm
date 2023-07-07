6d16d6ac7c69b91d1f304c14fab9e6a92c5bc1c2
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-07-06 23:27:23 -0300

    commit inicial

---
+++ public/home.php
@@ -0,0 +1,15 @@
+<?php
+include('header.php');
+?>
+<body>
+  <div class="container_page">
+    <?php require('../src/views/includes/navbar.php')?>
+    <?php require('../src/views/includes/tableContent.php')?>
+    <?php require('../src/views/includes/newUserModal.php')?>
+    <?php require('../src/views/includes/viewUserModal.php')?>
+  </div>
+  <!-- Meu arquivo JS -->
+  <script src="/teste-newm/public/js/script.js"></script>
+</body>
+
+</html>
